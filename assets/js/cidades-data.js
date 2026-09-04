e22c77bd964dd0dcb2d12142edab08299fd703e7
thiagojuniosalgado345-maker <thiagojuniosalgado345@gmail.com>
2026-09-04 20:08:26 -0300

    Create cidades-data.js

---
+++ assets/js/cidades-data.js
@@ -0,0 +1,12 @@
+const CIDADES = [
+  {
+    nome: "Montes Claros",
+    uf: "MG",
+    arquivo: "montes-claros.html"
+  },
+  {
+    nome: "Janaúba",
+    uf: "MG",
+    arquivo: "janauba.html"
+  }
+];
